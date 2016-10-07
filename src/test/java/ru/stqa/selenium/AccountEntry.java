f565d074ed016985c762c50ee341fba29f8f687c
IskoAnton <isko.anton@gmail.com>
2016-10-07 10:35:33 +0700

    2

--- src/test/java/ru/stqa/selenium/AccountEntry.java
+++ src/test/java/ru/stqa/selenium/AccountEntry.java
@@ -5,7 +5,9 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 
+
 public class AccountEntry extends TestNgTestBase {
+
 
   @Test
   public void testAccountEntry() throws Exception {
@@ -17,8 +19,12 @@ public class AccountEntry extends TestNgTestBase {
     driver.findElement(By.xpath("//input[@name='password']")).clear();
     driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
     driver.findElement(By.id("login")).click();
+    for (int second = 0;; second++) {
+    	if (second >= 60) fail("timeout");
+    	try { if ("".equals(driver.findElement(By.id("screen_1")).getText())) break; } catch (Exception e) {}
+    	Thread.sleep(1000);
+    }
     
-
   }
 
 }

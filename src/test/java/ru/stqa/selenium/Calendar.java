13579ae097a41b7e065ccce42388c115bcec565a
IskoAnton <isko.anton@gmail.com>
2016-10-07 17:06:37 +0700

    5

--- src/test/java/ru/stqa/selenium/Calendar.java
+++ src/test/java/ru/stqa/selenium/Calendar.java
@@ -6,27 +6,10 @@ import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 
 
-public class AccountEntry extends TestNgTestBase {
 
+public class Calendar extends TestNgTestBase {
+ 
 
-  @Test
-  public void testAccountEntry() throws Exception {
-    driver.get(baseUrl + "/");
-    String login = "tester5";
-    String password = "tester5";
-    driver.findElement(By.xpath("//input[@name='username']")).clear();
-    driver.findElement(By.xpath("//input[@name='username']")).sendKeys(login);
-    driver.findElement(By.xpath("//input[@name='password']")).clear();
-    driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
-    driver.findElement(By.id("login")).click();
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if ("".equals(driver.findElement(By.id("screen_1")).getText())) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
-    
-  }
-  
   @Test
   public void testCalendar() throws Exception {
     driver.get(baseUrl + "/online");
@@ -37,7 +20,7 @@ public class AccountEntry extends TestNgTestBase {
     }
 
     driver.findElement(By.xpath("//div[@id='jstree']//li/ul//li[1]//a")).click();
-    driver.findElement(By.linkText("–ê—Ä—Ö–∏–≤")).click();
+    driver.findElement(By.linkText("¿ıË‚")).click();
     driver.findElement(By.cssSelector("img.ui-datepicker-trigger")).click();
     assertTrue(isElementPresent(By.xpath("//div[@id='ui-datepicker-div' and contains(@style, 'display: block;')]")));
     driver.findElement(By.xpath("//div[@id='ui-datepicker-div']//tr[2]//td[4][contains(@data-handler, 'selectDay')]")).click();
@@ -48,14 +31,15 @@ public class AccountEntry extends TestNgTestBase {
     }
 
   }
-  
-  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
 
+ private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+ 
 }

673e6739d26c426243c63bdb4b51a01799fcf54d
IskoAnton <isko.anton@gmail.com>
2016-10-17 15:34:16 +0700

    1

--- src/test/java/languageCheck/LanguageAz.java
+++ src/test/java/languageCheck/LanguageAz.java
@@ -20,7 +20,7 @@ public class LanguageAz extends TestNgTestBase {
     } */
 
     Thread.sleep(5000);
-    assertEquals(driver.findElement(By.cssSelector("div.info-title")).getText(), "Şəxsi kabinet");
+    
     assertEquals(driver.findElement(By.xpath("//div[@id=\"login-box\"]//tr[1]//td[1]")).getText(), "Loqin");
     assertEquals(driver.findElement(By.xpath("//div[@id=\"login-box\"]//tr[2]//td")).getText(), "Parol");
     assertEquals(driver.findElement(By.id("login")).getAttribute("value"), "Daxil olun");
